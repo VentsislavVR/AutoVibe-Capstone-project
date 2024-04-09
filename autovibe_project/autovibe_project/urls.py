0ff0942368816c922c5a2522b345a2391e48937d
VentsislavVR <ventsislav.vichkov.rachev@gmail.com>
2024-04-09 22:05:39 +0300

    should work

--- autovibe_project/autovibe_project/urls.py
+++ autovibe_project/autovibe_project/urls.py
@@ -20,3 +20,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = Error404View.as_view()
+
+"adding a comment just for the commit"
